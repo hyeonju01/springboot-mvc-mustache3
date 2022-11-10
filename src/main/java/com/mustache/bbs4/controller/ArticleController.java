21ca9f3fd56d7a3c35bf65a9e8ec30eec49c19ed
hyeonju01 <tangled6648@gmail.com>
2022-11-10 14:12:10 +0900

    feat: findById(), list, update(), delete() 구현 및 view 완료

--- src/main/java/com/mustache/bbs4/controller/ArticleController.java
+++ src/main/java/com/mustache/bbs4/controller/ArticleController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -43,6 +44,38 @@ public class ArticleController {
         }
     }
 
+    // list 기능
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list";
+    }
+
+    // list 기능 (articles로 요청 들어올 경우)
+    @GetMapping("")
+    public String index() {
+        return "redirect:/articles/list";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(@PathVariable Long id, Model model){
+        Optional<Article> optionalArticle = articleRepository.findById(id);
+        if (!optionalArticle.isEmpty()) {
+            model.addAttribute("article", optionalArticle.get());
+            return "/articles/edit";
+        } else {
+            model.addAttribute("message", String.format("%가 없습니다.", id));
+            return "error";
+        }
+    }
+
+    @GetMapping("/{id}/delete")
+    public String edit(@PathVariable Long id) {
+        articleRepository.deleteById(id);
+        return "redirect:/articles";
+    }
+
     // add 기능 (INSERT INTO~)
     @PostMapping("")
     public String add(ArticleDto articleDto) {
@@ -50,5 +83,13 @@ public class ArticleController {
         Article savedArticle = articleRepository.save(articleDto.toEntity());
         log.info("generatedId: {}", savedArticle.getId());
         return String.format("redirect:/articles/%d", savedArticle.getId());
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable Long id, ArticleDto form, Model model) {
+        log.info("title: {} content: {}", form.getTitle(), form.getContent());
+        Article article = articleRepository.save(form.toEntity());
+        model.addAttribute("article", article);
+        return String.format("redirect:/articles/%d", article.getId());
     }
 }
